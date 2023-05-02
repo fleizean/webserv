1afcf31033bf16089242533e781940884ac9cc2b
Enes <nsyagz@gmail.com>
2023-05-02 21:42:43 +0300

    issue test

--- includes/include.hpp
+++ includes/include.hpp
@@ -18,9 +18,9 @@ using namespace std;
 #include "WebServer.hpp"
 #include "Server.hpp"
 
-bool isComment(const string& line, const set<string>& commentPrefixes, int& multi);
-string removeComments(const string& str);
-int fileToString(const string& filename, string& fileContents);
-bool isBracketBalanced(string fileContent);
-void parse_server(std::string _configContent);
-std::string	trim(std::string& line, const std::string& whitespace);
+bool			isComment(const string& line, const set<string>& commentPrefixes, int& multi);
+string			removeComments(const string& str);
+int				fileToString(const string& filename, string& fileContents);
+bool			isBracketBalanced(string fileContent);
+void			parse_server(std::string _configContent);
+std::string		trim(std::string& line, const std::string& whitespace);
